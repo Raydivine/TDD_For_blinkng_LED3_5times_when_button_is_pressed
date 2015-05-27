e5dd6d6e07b9fd7a7377e3cc5aa27adb4e1c32b1
Raydivine <raydivine00@gmail.com>
2015-05-27 20:45:36 +0800

    added 2 test case

--- test/test_blinkLED3.c
+++ test/test_blinkLED3.c
@@ -16,7 +16,7 @@ void test_blinkLED3FiveTimesWhenButtonPressed_given_INITIAL_and_NoPressedButton_
   int delay = 200;
   
   buttonPressed_IgnoreAndReturn(0);
-  turnOffLED3_IgnoreAndReturn(0);
+  turnOffLED3_IgnoreAndReturn(1);
 
   blinkLED3FiveTimesWhenButtonPressed(&state, delay);
   TEST_ASSERT_EQUAL(INITIAL,state);
@@ -27,7 +27,7 @@ void test_blinkLED3FiveTimesWhenButtonPressed_given_INITIAL_and_PressedButton_sh
   int delay = 200;
   
   buttonPressed_IgnoreAndReturn(1);
-  turnOffLED3_IgnoreAndReturn(0);
+  turnOffLED3_IgnoreAndReturn(1);
 
   blinkLED3FiveTimesWhenButtonPressed(&state, delay);
   TEST_ASSERT_EQUAL(LED3_OFF,state);
@@ -48,12 +48,30 @@ void test_blinkLED3FiveTimesWhenButtonPressed_given_LED3_ON_and_PressedButton_sh
   int delay = 200;
   
   buttonPressed_IgnoreAndReturn(1);
-  turnOffLED3_IgnoreAndReturn(0);
+  turnOffLED3_IgnoreAndReturn(1);
 
   blinkLED3FiveTimesWhenButtonPressed(&state, delay);
   TEST_ASSERT_EQUAL(LED3_OFF,state);
 }
 
+void test_blinkLED3FiveTimesWhenButtonPressed_given_LED3_OFF_and_NoPressedButton_should_remain_LED3_OFF(void){
+	State state = LED3_OFF;
+  int delay = 200;
+  
+  buttonPressed_IgnoreAndReturn(0);
 
+  blinkLED3FiveTimesWhenButtonPressed(&state, delay);
+  TEST_ASSERT_EQUAL(LED3_OFF,state);
+}
 
+void test_blinkLED3FiveTimesWhenButtonPressed_given_LED3_OFF_and_PressedButton_should_remain_LED3_ON(void){
+	State state = LED3_OFF;
+  int delay = 200;
+  
+  buttonPressed_IgnoreAndReturn(1);
+  turnOnLED3_IgnoreAndReturn(1);
+
+  blinkLED3FiveTimesWhenButtonPressed(&state, delay);
+  TEST_ASSERT_EQUAL(LED3_ON,state);
+}
 
